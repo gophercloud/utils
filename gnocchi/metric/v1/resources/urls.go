39f741e7676b5f44c7c12f8bef6e2e741fed2719
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-01-10 23:21:02 +0300

    Update Gnocchi resources urls
    Add a generic rootURL that can be used both in list and create requests.

--- gnocchi/metric/v1/resources/urls.go
+++ gnocchi/metric/v1/resources/urls.go
@@ -4,9 +4,13 @@ import "github.com/gophercloud/gophercloud"
 
 const resourcePath = "resource"
 
-func listURL(c *gophercloud.ServiceClient, resourceType string) string {
+func rootURL(c *gophercloud.ServiceClient, resourceType string) string {
 	if resourceType == "" {
 		resourceType = "generic"
 	}
 	return c.ServiceURL(resourcePath, resourceType)
+}
+
+func listURL(c *gophercloud.ServiceClient, resourceType string) string {
+	return rootURL(c, resourceType)
 }
