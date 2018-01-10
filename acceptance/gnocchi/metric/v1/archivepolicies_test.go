d4410b8e01b3b7573941266dade44502d94b9824
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-01-10 10:28:14 +0300

    Add Gnocchi archive policies list acceptance test

---
+++ acceptance/gnocchi/metric/v1/archivepolicies_test.go
@@ -0,0 +1,30 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	"github.com/gophercloud/utils/acceptance/clients"
+	"github.com/gophercloud/utils/gnocchi/metric/v1/archivepolicies"
+)
+
+func TestArchivePoliciesList(t *testing.T) {
+	client, err := clients.NewGnocchiV1Client()
+	if err != nil {
+		t.Fatalf("Unable to create a Gnocchi client: %v", err)
+	}
+
+	allPages, err := archivepolicies.List(client).AllPages()
+	if err != nil {
+		t.Fatalf("Unable to list archive policies: %v", err)
+	}
+
+	allArchivePolicies, err := archivepolicies.ExtractArchivePolicies(allPages)
+	if err != nil {
+		t.Fatalf("Unable to extract archive policies: %v", err)
+	}
+
+	for _, archivePolicy := range allArchivePolicies {
+		tools.PrintResource(t, archivePolicy)
+	}
+}
