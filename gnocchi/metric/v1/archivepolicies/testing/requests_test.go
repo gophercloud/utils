14f07363124307ce3a9d604f83ae594485596f76
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-01-11 17:28:20 +0300

    Move fake Gnocchi client to "gnocchi/testhelper/client/fake.go"
    That scheme would match "gophercloud/gophercloud".

--- gnocchi/metric/v1/archivepolicies/testing/requests_test.go
+++ gnocchi/metric/v1/archivepolicies/testing/requests_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gophercloud/gophercloud/pagination"
 	th "github.com/gophercloud/gophercloud/testhelper"
 	"github.com/gophercloud/utils/gnocchi/metric/v1/archivepolicies"
-	fake "github.com/gophercloud/utils/gnocchi/metric/v1/common"
+	fake "github.com/gophercloud/utils/gnocchi/testhelper/client"
 )
 
 func TestListArchivePolicies(t *testing.T) {
--- gnocchi/testhelper/client/fake.go
+++ gnocchi/testhelper/client/fake.go
@@ -1,4 +1,4 @@
-package common
+package client
 
 import (
 	"github.com/gophercloud/gophercloud"
