4d65305fce6b72fecc5b3e19de49e7b323cf4d8d
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-01-13 23:04:33 +0300

    Add Gnocchi resources list acceptance test

---
+++ acceptance/gnocchi/metric/v1/resources_test.go
@@ -0,0 +1,32 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/gophercloud/gophercloud/acceptance/tools"
+	"github.com/gophercloud/utils/acceptance/clients"
+	"github.com/gophercloud/utils/gnocchi/metric/v1/resources"
+)
+
+func TestResourcesList(t *testing.T) {
+	client, err := clients.NewGnocchiV1Client()
+	if err != nil {
+		t.Fatalf("Unable to create a Gnocchi client: %v", err)
+	}
+
+	opts := resources.ListOpts{}
+	resourceType := ""
+	allPages, err := resources.List(client, opts, resourceType).AllPages()
+	if err != nil {
+		t.Fatalf("Unable to list resources: %v", err)
+	}
+
+	allResources, err := resources.ExtractResources(allPages)
+	if err != nil {
+		t.Fatalf("Unable to extract resources: %v", err)
+	}
+
+	for _, resource := range allResources {
+		tools.PrintResource(t, resource)
+	}
+}
