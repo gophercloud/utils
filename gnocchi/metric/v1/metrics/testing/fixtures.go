7a266544d77b69a9581f9d9314e17d6ddfcf45da
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-01-15 07:56:12 +0300

    Implement Gnocchi metric get (#15)
    * Implement Gnocchi metric get
    
    Add a Gnocchi metric get request and an unit test.
    
    * Fix Gnocchi metrics unit tests
    
    Fix TestGet assert structures and fix imports in the requests_test.
    
    * Fix Gnocchi metrics unit tests
    
    Fix imports and structs.
    
    * Fix Gnocchi metrics tests list and get fixtures
    
    * Fix Gnocchi metric get unit test
    
    Fix resource part representation.

--- gnocchi/metric/v1/metrics/testing/fixtures.go
+++ gnocchi/metric/v1/metrics/testing/fixtures.go
@@ -9,29 +9,29 @@ import (
 const MetricsListResult = `[
     {
         "archive_policy": {
-        "aggregation_methods": [
-            "max",
-            "min"
-        ],
-        "back_window": 0,
-        "definition": [
-            {
-                "granularity": "1:00:00",
-                "points": 2304,
-                "timespan": "96 days, 0:00:00"
-            },
-            {
-                "granularity": "0:05:00",
-                "points": 9216,
-                "timespan": "32 days, 0:00:00"
-            },
-            {
-                "granularity": "1 day, 0:00:00",
-                "points": 400,
-                "timespan": "400 days, 0:00:00"
-            }
-        ],
-        "name": "precise"
+            "aggregation_methods": [
+                "max",
+                "min"
+            ],
+            "back_window": 0,
+            "definition": [
+                {
+                    "granularity": "1:00:00",
+                    "points": 2304,
+                    "timespan": "96 days, 0:00:00"
+                },
+                {
+                    "granularity": "0:05:00",
+                    "points": 9216,
+                    "timespan": "32 days, 0:00:00"
+                },
+                {
+                    "granularity": "1 day, 0:00:00",
+                    "points": 400,
+                    "timespan": "400 days, 0:00:00"
+                }
+            ],
+            "name": "precise"
         },
         "created_by_project_id": "e9dc821ca664406e981820a477e9a761",
         "created_by_user_id": "a23c5b98d42d4df3b961e54d5167eb6d",
@@ -138,3 +138,49 @@ var Metric2 = metrics.Metric{
 	ResourceID:         "c5dc0c47-f43c-425c-a82f-44d61ee91175",
 	Unit:               "ns",
 }
+
+// MetricGetResult represents a raw server response from a server to a get request.
+const MetricGetResult = `
+{
+    "archive_policy": {
+        "aggregation_methods": [
+            "mean",
+            "sum"
+        ],
+        "back_window": 12,
+        "definition": [
+            {
+                "granularity": "1:00:00",
+                "points": 2160,
+                "timespan": "90 days, 0:00:00"
+            },
+            {
+                "granularity": "1 day, 0:00:00",
+                "points": 200,
+                "timespan": "200 days, 0:00:00"
+            }
+        ],
+        "name": "not_so_precise"
+    },
+    "created_by_project_id": "c6b68a6b413648b0a0eb191bf3222f4d",
+    "created_by_user_id": "cb072aacdb494419aeeba5f1c62d1a65",
+    "creator": "cb072aacdb494419aeeba5f1c62d1a65:c6b68a6b413648b0a0eb191bf3222f4d",
+    "id": "0ddf61cf-3747-4f75-bf13-13c28ff03ae3",
+    "name": "network.incoming.packets.rate",
+    "resource": {
+        "created_by_project_id": "c6b68a6b413648b0a0eb191bf3222f4d",
+        "created_by_user_id": "cb072aacdb494419aeeba5f1c62d1a65",
+        "creator": "cb072aacdb494419aeeba5f1c62d1a65:c6b68a6b413648b0a0eb191bf3222f4d",
+        "ended_at": null,
+        "id": "75274f99-faf6-4112-a6d5-2794cb07c789",
+        "original_resource_id": "75274f99-faf6-4112-a6d5-2794cb07c789",
+        "project_id": "4154f08883334e0494c41155c33c0fc9",
+        "revision_end": null,
+        "revision_start": "2018-01-08T00:59:33.767815+00:00",
+        "started_at": "2018-01-08T00:59:33.767795+00:00",
+        "type": "compute_instance_network",
+        "user_id": "bd5874d666624b24a9f01c128871e4ac"
+    },
+    "unit": "packet/s"
+}
+`
--- gnocchi/metric/v1/metrics/testing/requests_test.go
+++ gnocchi/metric/v1/metrics/testing/requests_test.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/gophercloud/gophercloud/pagination"
 	th "github.com/gophercloud/gophercloud/testhelper"
+	"github.com/gophercloud/utils/gnocchi/metric/v1/archivepolicies"
 	"github.com/gophercloud/utils/gnocchi/metric/v1/metrics"
+	"github.com/gophercloud/utils/gnocchi/metric/v1/resources"
 	fake "github.com/gophercloud/utils/gnocchi/testhelper/client"
 )
 
@@ -48,4 +51,64 @@ func TestList(t *testing.T) {
 	if count != 1 {
 		t.Errorf("Expected 1 page, got %d", count)
 	}
+}
+
+func TestGet(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v1/metric/0ddf61cf-3747-4f75-bf13-13c28ff03ae3", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, MetricGetResult)
+	})
+
+	s, err := metrics.Get(fake.ServiceClient(), "0ddf61cf-3747-4f75-bf13-13c28ff03ae3").Extract()
+	th.AssertNoErr(t, err)
+
+	th.AssertDeepEquals(t, s.ArchivePolicy, archivepolicies.ArchivePolicy{
+		AggregationMethods: []string{
+			"mean",
+			"sum",
+		},
+		BackWindow: 12,
+		Definition: []archivepolicies.ArchivePolicyDefinition{
+			{
+				Granularity: "1:00:00",
+				Points:      2160,
+				TimeSpan:    "90 days, 0:00:00",
+			},
+			{
+				Granularity: "1 day, 0:00:00",
+				Points:      200,
+				TimeSpan:    "200 days, 0:00:00",
+			},
+		},
+		Name: "not_so_precise",
+	})
+	th.AssertEquals(t, s.CreatedByProjectID, "c6b68a6b413648b0a0eb191bf3222f4d")
+	th.AssertEquals(t, s.CreatedByUserID, "cb072aacdb494419aeeba5f1c62d1a65")
+	th.AssertEquals(t, s.Creator, "cb072aacdb494419aeeba5f1c62d1a65:c6b68a6b413648b0a0eb191bf3222f4d")
+	th.AssertEquals(t, s.ID, "0ddf61cf-3747-4f75-bf13-13c28ff03ae3")
+	th.AssertEquals(t, s.Name, "network.incoming.packets.rate")
+	th.AssertDeepEquals(t, s.Resource, resources.Resource{
+		CreatedByProjectID: "c6b68a6b413648b0a0eb191bf3222f4d",
+		CreatedByUserID:    "cb072aacdb494419aeeba5f1c62d1a65",
+		Creator:            "cb072aacdb494419aeeba5f1c62d1a65:c6b68a6b413648b0a0eb191bf3222f4d",
+		ID:                 "75274f99-faf6-4112-a6d5-2794cb07c789",
+		OriginalResourceID: "75274f99-faf6-4112-a6d5-2794cb07c789",
+		ProjectID:          "4154f08883334e0494c41155c33c0fc9",
+		RevisionStart:      time.Date(2018, 1, 8, 00, 59, 33, 767815000, time.UTC),
+		RevisionEnd:        time.Time{},
+		StartedAt:          time.Date(2018, 1, 8, 00, 59, 33, 767795000, time.UTC),
+		EndedAt:            time.Time{},
+		Type:               "compute_instance_network",
+		UserID:             "bd5874d666624b24a9f01c128871e4ac",
+		Extra:              map[string]interface{}{},
+	})
+	th.AssertEquals(t, s.Unit, "packet/s")
 }
