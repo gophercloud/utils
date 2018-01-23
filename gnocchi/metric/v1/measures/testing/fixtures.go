8bb92e86bca56b9d48daa414b8c527fe96ec009c
Andrei Ozerov <andrei.ozerov92@gmail.com>
2018-01-23 22:03:44 +0300

    Fix Gnocchi measures list unit test
    Update results fixtures.

--- gnocchi/metric/v1/measures/testing/fixtures.go
+++ gnocchi/metric/v1/measures/testing/fixtures.go
@@ -1,6 +1,10 @@
 package testing
 
-import "github.com/gophercloud/utils/gnocchi/metric/v1/measures"
+import (
+	"time"
+
+	"github.com/gophercloud/utils/gnocchi/metric/v1/measures"
+)
 
 // MeasuresListResult represents a raw server response from a server to a List call.
 const MeasuresListResult = `
@@ -26,17 +30,17 @@ const MeasuresListResult = `
 // ListArchivePoliciesExpected represents an expected repsonse from a List request.
 var ListArchivePoliciesExpected = []measures.Measure{
 	{
-		TimeStamp:   "2018-01-10T12:00:00+00:00",
+		TimeStamp:   time.Date(2018, 1, 10, 12, 0, 0, 0, time.UTC),
 		Granularity: 3600.0,
 		Value:       15.0,
 	},
 	{
-		TimeStamp:   "2018-01-10T13:00:00+00:00",
+		TimeStamp:   time.Date(2018, 1, 10, 13, 0, 0, 0, time.UTC),
 		Granularity: 3600.0,
 		Value:       10.0,
 	},
 	{
-		TimeStamp:   "2018-01-10T14:00:00+00:00",
+		TimeStamp:   time.Date(2018, 1, 10, 14, 0, 0, 0, time.UTC),
 		Granularity: 3600.0,
 		Value:       20.0,
 	},
