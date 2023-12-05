4f57841d2f12b5ddeecea5f50bd66124f3d9e302
Lars Lehtonen <lars.lehtonen@gmail.com>
2023-12-05 12:32:06 -0800

    openstack/clientconfig: fix dropped errors

--- openstack/clientconfig/requests.go
+++ openstack/clientconfig/requests.go
@@ -330,7 +330,7 @@ func GetCloudFromYAML(opts *ClientOpts) (*Cloud, error) {
 
 	cloud.Interface = ""
 
-	return cloud, nil
+	return cloud, err
 }
 
 // AuthOptions creates a gophercloud.AuthOptions structure with the
